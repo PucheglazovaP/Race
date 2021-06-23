e92471e7c325bac47390b0456ac9c9acbfd5dba6
Polina Pucheglazova <polina9913@mail.ru>
2021-06-24 06:52:01 +0700

    создан конфиг. файл с данными в формате json

---
+++ config.js
@@ -0,0 +1,37 @@
+export default{
+  "vehicles": {
+    "cars" : [
+      {
+        "name":"car-1" ,
+        "speed": 0.02,
+        "tireFlatProbability": 0.007,
+        "numOfPeople": 2
+      },
+      {
+        "name":"car-2" ,
+        "speed": 0.025,
+        "tireFlatProbability": 0.007,
+        "numOfPeople": 3
+      }
+      ],
+      "trucks" : [
+        {
+        "name":"truck-1",
+        "speed": 0.01,
+        "tireFlatProbability": 0.006,
+        "cargoWeight": 300
+      }
+        ],
+      "bikes": [
+        {
+        "name":"bike-1",
+        "speed": 0.03,
+        "tireFlatProbability": 0.005,
+        "hasSidecar": false
+      }
+      ]
+  },
+  "circle": {
+    "circleLength":1570.796
+  }
+}
