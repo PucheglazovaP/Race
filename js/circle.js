4c84a25ac27b5e419f83e3aa22258dd69ac953b1
Polina Pucheglazova <polina9913@mail.ru>
2021-06-25 20:30:48 +0700

    в классе круга реализован конструктор и метод отрисовки на странице круга

--- js/circle.js
+++ js/circle.js
@@ -1,1 +1,18 @@
-//Планируется создание класса круга, опредедение в нем свойств и методов
+export class Circle {
+    constructor(circleLength) {
+        this.circleLength = circleLength; //длина круга
+        this.radius=this.circleLength/(2*Math.PI); //вычисление радиуса
+        this.circle = document.querySelector('.round');
+        this.baseX;
+        this.baseY;
+        
+        this.createCircle();
+    }
+    createCircle(){
+        this.circle.style.width=2*this.radius + "px";
+        this.circle.style.height=2*this.radius + "px";
+        this.baseX = (2*this.radius)/2;
+        this.baseY = (2*this.radius)/2;
+        this.circle.style.display="block";
+    }
+}
